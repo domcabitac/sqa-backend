bebe96389ff52a83d409b30fffdaa9603b5672c1
leon-chow <leon.chow1@uoit.net>
2020-03-07 21:48:29 -0500

    Add some functional logic for set methods, add todo instructions for a few functions

--- src/backend/OutputWriter.java
+++ src/backend/OutputWriter.java
@@ -14,28 +14,31 @@ public class OutputWriter {
     public String OutputFilepath;
 
     public void setMergedTransactions(Vector<String> transactionBuffer) {
-
+        this.transactionBuffer = transactionBuffer;
     }
     
     public void setOldItems(Vector<String> oldItemBuffer) {
-
+        this.oldItemBuffer = oldItemBuffer;
     } 
     
     public void setOldUser(Vector<String> oldUserBuffer) {
-
+        this.oldUserBuffer = oldUserBuffer;
     } 
     public void writeNewUsers(Vector<String> oldUserBuffer, Vector<String> transactionsBuffer, String OutputFilepath) {
-
+        // TODO: Using the old user buffer, perform transactions from transactionBuffer marked for users, either create, delete or edit a user 
     }
     
     public void writeNewItems(Vector<String> oldItemBuffer, Vector<String> transactionBuffer, String OutputFilepath) {
-
+        // TODO: Using the old item buffer, perform transactions from transactionBuffer marked for items, either create or edit a bid. 
+        // Also decrement all auction days in the file by 1
     }
     public String determineTransactionType(String currentTransaction) {
+        // TODO: takes in the current transaction from the buffer, and determines if its user or items file affected
         return "Works";
     } 
     
     public String logInformation(String currentTransaction, String OutputFilepath, Vector<String> ErrorMessages) {
+        // TODO: if an error has occurred, log the information to a file and print it out to a user
         return "Works";
     } 
 
