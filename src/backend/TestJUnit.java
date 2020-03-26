7b2e8c8013a4fdd4073a74cc6c7b519d93828ee6
Dominic Cabitac <dominic.cabitac@uoit.net>
2020-03-26 18:11:01 -0400

    Readers Test Done

--- src/backend/TestJUnit.java
+++ src/backend/TestJUnit.java
@@ -1,12 +1,10 @@
-// Currently does not work, not running on Mac OSX
-
-import org.junit.Test;
+import org.junit.*;
 import java.io.*;
 import java.util.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
-public class TestJUnit {
+public class TestJunit {
     public Vector<String> oldItemBuffer = new Vector<String>();
     public Vector<String> oldUserBuffer = new Vector<String>();
     public Vector<String> transactionsBuffer = new Vector<String>();
@@ -17,30 +15,34 @@ public class TestJUnit {
     @Test
     public void ReadingUserFileTest() {
         OldUserFileReader u = new OldUserFileReader();
+        String testUser = "[Bob             AA 0000005.00 password, Test            FS 0020005.10 passward, John            FS 0000000.00 passward, Mart            BS 0000000.00 password, bobby           SS 0000000.00 password, END]";
 
-        assertEquals("", u.readUserFile("current-user-accounts.txt"));
+        assertEquals(testUser, u.readUserFile("current-user-accounts.txt").toString());
     }
 
     @Test
     public void ReadingItemFileTest() {
         OldItemFileReader i = new OldItemFileReader();
+        String testItem = "[macbook pro 16 i7 silver  John            Bob             900 700.00, Not a less paul guitaree  John            NULL            050 699.99, UOIT Water Bottle         John            NULL            020 599.99, UOIT Backpack             john            Bob             100 700.00, UOIT Backpack             john            Bob             100 700.00, END]";
 
-        assertEquals("", i.readItemFile("available-items-test.txt"));
+        assertEquals(testItem, i.readItemFile("available-items-test.txt").toString());
     }
 
     @Test
     public void ReadingTransFileTest() {
         TransactionReader t = new TransactionReader();
-
-        assertEquals("", t.readMergedTransaction("daily-transactions.txt"));
+        String testTrans = "[04 macbook pro 16 i7 silver  john            Bob             700.00, 03 UOIT Backpack             john            Bob             100 700.00]";
+        
+        assertEquals(testTrans, t.readMergedTransaction("daily-transactions.txt").toString());
     }
 
-    @Test
-    public void BuffingNewUserTest() {
-        OutputWriter w = new OutputWriter();
+    // Not complete yet in OutputWriter.java
+    // @Test
+    // public void BuffingNewUserTest() {
+    //     OutputWriter w = new OutputWriter();
 
-        assertEquals("", w.bufferNewUsers());
-    }
+    //     assertEquals("", w.bufferNewUsers());
+    // }
 
     @Test
     public void BuffingNewItemTest() {
@@ -49,25 +51,25 @@ public class TestJUnit {
         assertEquals("", w.bufferNewItems());
     }
 
-    @Test
-    public void WritingNewItemsTest() {
-        OldItemFileReader i = new OldItemFileReader();
-        OutputWriter w = new OutputWriter();
+    // @Test
+    // public void WritingNewItemsTest() {
+    //     OldItemFileReader i = new OldItemFileReader();
+    //     OutputWriter w = new OutputWriter();
 
-        Vector<String> oldItemBuffer = i.readItemFile("available-items-test.txt");
-
-
-        assertEquals("Writing new items...", w.writeNewItems(oldItemBuffer, "available-items-test.txt"));
-    }
-
-    @Test
-    public void WritingNewUsersTest() {
-        OldUserFileReader u = new OldUserFileReader();
-        OutputWriter w = new OutputWriter();
-
-        Vector<String> oldItemBuffer = u.readUserFile("current-user-accounts.txt");
+    //     Vector<String> oldItemBuffer = i.readItemFile("available-items-test.txt");
 
 
-        assertEquals("Writing new users...", w.writeNewUsers(oldUserBuffer, "current-user-accounts.txt"));
-    }
+    //     assertEquals("Writing new items...", w.writeNewItems(oldItemBuffer, "available-items-test.txt"));
+    // }
+
+    // @Test
+    // public void WritingNewUsersTest() {
+    //     OldUserFileReader u = new OldUserFileReader();
+    //     OutputWriter w = new OutputWriter();
+
+    //     Vector<String> oldItemBuffer = u.readUserFile("current-user-accounts.txt");
+
+
+    //     assertEquals("Writing new users...", w.writeNewUsers(oldUserBuffer, "current-user-accounts.txt"));
+    // }
 }
